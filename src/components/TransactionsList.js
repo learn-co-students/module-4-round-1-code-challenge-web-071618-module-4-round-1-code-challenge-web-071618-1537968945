82584222e09c12354e73662d5a468a3e0590d1fd
trisjones <trisjones04@gmail.com>
2018-09-26 10:39:15 -0400

    Almost finished

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
-
+const TransactionsList = (props) => {
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -28,7 +28,7 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {props.transactions.map(transaction=> <Transaction key={transaction.id} transaction={ {...transaction} }/>)}
 
       </tbody>
     </table>

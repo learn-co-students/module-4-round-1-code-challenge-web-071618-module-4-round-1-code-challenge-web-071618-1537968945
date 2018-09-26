03c1491c5e129ce5d68a1fe05a5fb5f21daa7702
yunglleung1 <yungl.leung1@gmail.com>
2018-09-26 10:39:53 -0400

    Halfway Done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -11,21 +11,50 @@ class AccountContainer extends Component {
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
+    this.state = {
+      transactions: [],
+      searchTerm: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event) {
-    // your code here
+    this.setState({
+      searchTerm: event.target.value
+    })
+
+    // let updateList = this.state.transactions.map((transaction) => {
+    //   if(transaction.description === this.state.searchTerm) {
+    //       console.log(this.state.transaction)
+    //   }
+    // })
+    // console.log(this.state.transactions)
+    console.log(event.target.value)
+  }
+
+  componentDidMount(){
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        transactions: data
+      })
+    })
   }
 
   render() {
-
-    return (
-      <div>
-        <Search />
-        <TransactionsList />
-      </div>
-    )
+    // console.log(this.fetchData())
+    // console.log(this.state.transactions)
+    if(this.state.transactions.length > 0) {
+      return (
+        <div>
+          <Search handleChange={this.handleChange}/>
+          <TransactionsList list={this.state.transactions}/>
+        </div>
+      )
+    } else {
+      return (<div>Loading</div>)
+    }
   }
 }
 
